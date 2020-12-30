998660c73ec3bd2a7b584b08ce420f64f0284d9d
eugenepro2 <eugenepro@icloud.com>
2020-12-30 12:38:19 +0200

    Finish project config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/index.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
